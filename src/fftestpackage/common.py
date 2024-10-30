8c5dbaafdd3804940f34cb2b4febf4a85b302340
irideselby <172918893+irideselby@users.noreply.github.com>
2024-10-30 18:08:04 +0100

    Add a file for common functions
    Some functions will be used throughout the package and should be stored
    in a central place.

---
+++ src/fftestpackage/common.py
@@ -0,0 +1,20 @@
+"""A module containing functions that are used throughout the package."""
+
+
+def multiply_numbers(number_1: float, number_2: float) -> float:
+    """Multiply two given numbers.
+
+    Parameters
+    ----------
+    number_1
+        A number to be multiplied.
+
+    number_2
+        A number to be multiplied.
+
+    Returns
+    -------
+        A number calculated from multiplying to numbers.
+    """
+
+    return number_1 * number_2
