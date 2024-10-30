7a0f3bed6bdfe40c37302843ed812834d0e8903a
irideselby <172918893+irideselby@users.noreply.github.com>
2024-10-30 19:13:40 +0100

    Add testing environment and first unit test

---
+++ tests/test_multiply_numbers.py
@@ -0,0 +1,10 @@
+"""Tests for the multiply_numbers function from common.py."""
+
+from fftestpackage.common import multiply_numbers
+
+
+def test_multiply_numbers():
+    """Test correct output of multiply_numbers function."""
+
+    assert multiply_numbers(1, 2) == 2
+
